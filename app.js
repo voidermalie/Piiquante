54225d3f4b4c7496b747d33d0fa88f00f10ead89
voidermalie <lestarfanny@gmail.com>
2023-03-01 18:36:19 +0100

    crud test fait

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
 // ----------------------------------------Express------------------------------------------------- 
-const express = require('express'); //importer express
+const express = require('express');                             //importer express
 
-const app = express(); //appeler la méthode permet de créer une app Express
+const app = express();              //appeler cette méthode permet de créer une app Express
 
+//----------------------------------------Middlewares----------------------------------------------
+
+app.use(express.json()); //intercèpte toutes les requêtes ayant pour content-type: json et met à disp pour l'objet requête
+
+//----handle CORS error --------------------------------------------------------------------------
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+//importation du schéma sauce
+const Sauce = require('./models/sauce');
+
+// POST
+app.post((req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauce({
+      ...req.body
+    });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-    res.json({ message : 'Votre requête a bien été reçue !'});
-    next();
+// PUT (modification)
+app.put('/api/sauces/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
+// DELETE 
+app.delete('/api/sauces/:id', (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
 });
 
-module.exports = app; // exporter pour y accéder des autres fichiers, notamment le serveur node
+// GET
+//Une sauce spécifique
+app.get('/api/sauces/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(404).json({ error }));
+});
+
+//Toutes les sauces
+app.get('/api/sauces', (req, res, next) => {
+    Sauce.find()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app; // exporter pour y accéder depuis les autres fichiers, notamment le serveur node
 
 // ----------------------------------------MongoDB------------------------------------------------- 
 // Connexion à la base de données
